c651c73bf9a0b133e9f6025deb08e8774ba84860
QviZet <mgv638@mail.ru>
2025-03-18 04:33:31 +0300

    comment hello_world.cpp to patch1

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::string name;
     std::cout << "User's name:" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    std::cin >> name; //Input username
+    std::cout << "Hello world from " << name << std::endl; //Print output
 }
